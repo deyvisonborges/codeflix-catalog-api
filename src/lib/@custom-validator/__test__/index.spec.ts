d450e053a9617c90b892e8a8be7565ae9d1dfaeb
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-10-31 14:56:10 -0300

    change message from hasMin validator

--- src/lib/@custom-validator/__test__/index.spec.ts
+++ src/lib/@custom-validator/__test__/index.spec.ts
@@ -3,7 +3,21 @@ import { Validator } from '..';
 describe(`Lib - Validator`, () => {
   it(`test validation`, () => {
     const validation = new Validator<{ name: string; description: string }>();
-    validation.isRequired(`name`, `this is a value`);
-    validation.isRequired(`description`, 'this is a value');
+    validation.isRequired(`name`, ``);
+    validation.hasMin(`name`, `val`, 4);
+    validation.hasMin(`name`, `v`, 2);
+    validation.isRequired(`description`, '');
+
+    console.log(validation.errors);
+  });
+
+  it(`should return a invalid email`, () => {
+    const validation = new Validator<{ name: string; description: string }>();
+    expect(validation.isValidEmail(`fakeemail.com`)).toEqual(false);
+  });
+
+  it(`should return a valid email`, () => {
+    const validation = new Validator<{ name: string; description: string }>();
+    expect(validation.isValidEmail(`vali@gmail.com`)).toEqual(true);
   });
 });
--- src/lib/@custom-validator/index.ts
+++ src/lib/@custom-validator/index.ts
@@ -1,22 +1,29 @@
 export class Validator<Entity> {
-  errors = new Map<keyof Entity, string[]>();
+  errors = new Map<keyof Entity, string | string[]>();
 
   addError(attribute: keyof Entity, message: string): void {
-    if (this.errors.has(attribute)) {
-      this.errors.get(attribute)?.push(message);
+    const existingError = this.errors.get(attribute);
+    if (existingError) {
+      if (typeof existingError === 'string') {
+        this.errors.set(attribute, [existingError, message]);
+      } else if (Array.isArray(existingError)) {
+        existingError.push(message);
+      }
     } else {
-      this.errors.set(attribute, [message]);
+      this.errors.set(attribute, message);
     }
   }
 
-  hasMin(
-    attribute: keyof Entity,
-    value: string,
-    length: number,
-    message: string,
-  ) {
+  hasMin(attribute: keyof Entity, value: string, length: number) {
     if (value !== null && value.length < length) {
-      this.addError(attribute, message);
+      this.addError(
+        attribute,
+        'attribute ' +
+          attribute.toString() +
+          ' must have at least ' +
+          value +
+          ' characters.',
+      );
     }
   }
 
@@ -38,5 +45,10 @@ export class Validator<Entity> {
         'attribute ' + attribute.toString() + ' is required',
       );
     }
+  }
+
+  isValidEmail(email: string) {
+    const pattern = new RegExp('^(.+)@(\\S+)$');
+    return pattern.test(email);
   }
 }

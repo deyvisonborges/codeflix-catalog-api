958f2aa5935ca2a67c443d074055d99125f2206b
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-11-02 22:57:06 -0300

    add category integration module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CategoriesModule } from './categories/categories.module';
+import { CategoriesModule } from './module/categories/categories.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [CategoriesModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })

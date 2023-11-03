9439b389a4ce5e2f2009f51b133921ae9163cb53
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-11-03 18:20:13 -0300

    resolve prisma module

--- src/database/prisma/prisma.module.ts
+++ src/database/prisma/prisma.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { PrismaModule as NestPrismModule } from 'nestjs-prisma';
 
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
 })
-export class PrismaModule extends NestPrismModule {}
+export class PrismaModule {}
--- src/database/prisma/prisma.service.spec.ts
+++ src/database/prisma/prisma.service.spec.ts
@@ -1,15 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from './prisma.service';
+import { ConfigService } from '@nestjs/config';
+import { EnvironmentVariables } from 'src/config/config.types';
 
 describe('PrismaService', () => {
   let service: PrismaService;
+  let configService: ConfigService<EnvironmentVariables>; // Declare uma instância de ConfigService com o tipo genérico
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PrismaService],
+      providers: [
+        PrismaService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: () => 'DB_URL', // Simule o método get para retornar a URL da sua DB
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<PrismaService>(PrismaService);
+    configService =
+      module.get<ConfigService<EnvironmentVariables>>(ConfigService); // Obtenha a instância do ConfigService
   });
 
   it('should be defined', () => {

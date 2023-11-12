12923968f978accbd6be4fd7a03e21df8898643c
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-11-12 00:45:15 -0300

    add wrapper data interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WrapperDataInterceptor } from './interceptors/wrapper-data/wrapper-data.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new WrapperDataInterceptor());
   await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
 }
 bootstrap();
